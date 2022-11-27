5e0a32758d2053ee90aad293a0f528bd70dfbce8
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2022-11-27 11:01:01 +0200

    Fixes

--- src/components/Molecules/ImageCard/index.tsx
+++ src/components/Molecules/ImageCard/index.tsx
@@ -1,17 +1,24 @@
 import { useRef } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ImageCardWrapper } from './ImageCard.styles'
 import type { ImageCardProps } from './ImageCard.types'
 import type { FC } from 'react'
 import ImageDescription from '../../Atoms/ImageDescription'
 import Image from '../../Atoms/Image'
+import { Context } from '../../../utils/context'
 
 const ImageCard: FC<ImageCardProps> = ({ image, description }) => {
+  const [imageHeight, setImageHeight] = useState(0)
+  const [context] = useContext(Context)
   const ref = useRef<HTMLDivElement>(null)
   const handleImageCardHeight = (imageHeight: number): number => {
     const imageDescriptionHeight = 120
 
     return imageHeight + imageDescriptionHeight
   }
+  useEffect(() => {
+    setImageHeight(handleImageCardHeight(ref ? ref.current ? Number(ref.current.children[0].getAttribute('data-height')) : 0 : 0))
+  }, [imageHeight, context])
 
   return (
     <ImageCardWrapper className="mm-masonry__item" cardHeight={handleImageCardHeight(ref ? ref.current ? Number(ref.current.children[0].getAttribute('data-height')) : 0 : 0)}
