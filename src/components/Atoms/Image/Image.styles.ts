5178502c0f2baa7848164965d9b6de196871ae72
Mariana Kyrkosh <mariana.kyrkosh@gmail.com>
2022-11-28 23:54:24 +0200

    Styling fixes

--- src/components/Atoms/Image/Image.styles.ts
+++ src/components/Atoms/Image/Image.styles.ts
@@ -5,18 +5,6 @@ export const StyledImage = styled.img`
   width: 100%;
   border-radius: ${radius.regular};
 
-  @keyframes fadeIn {
-    99% {
-      visibility: hidden;
-    }
-    100% {
-      visibility: visible;
-    }
-  }
-  animation: 1s fadeIn;
-  animation-fill-mode: forwards;
-  visibility: hidden;
-
   @media (min-width: ${breakpoints.m}) {
     width: 100%;
     height: auto;
